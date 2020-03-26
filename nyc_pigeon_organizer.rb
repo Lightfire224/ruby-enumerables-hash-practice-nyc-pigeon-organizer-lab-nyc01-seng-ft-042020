19cd1c15da4943c3fb31806b6dcb83d9cae59705
Elias Taveras <eliastave12@gmail.com>
2020-03-26 21:09:46 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,21 +1,21 @@
 def nyc_pigeon_organizer(pigeon_data)
-  hash = {}
-  data.each do |property, hash|
-    hash.each do |attribute, array|
-      array.each do |name|
-        if !new_hash.has_key?(name)
-          new_hash[name] = {}
+    hash = {}
+    data.each do |test, hash|
+        hash.each do |features, array|
+            array.each do |name|
+                if !hash.has_key?(name)
+                    hash[name] = {}
+                end
+                
+                if !hash[name].has_key?(test)
+                    hash[name][test] = []
+                end
+                
+                if !hash[name][test].include?(features)
+                    hash[name][test] << features.to_s
+                end
+            end
         end
-
-        if !new_hash[name].has_key?(property)
-          new_hash[name][property] = []
-        end
-
-        if !new_hash[name][property].include?(attribute)
-          new_hash[name][property] << attribute.to_s
-        end
-      end
     end
-  end
-  new_hash
+    hash
 end
